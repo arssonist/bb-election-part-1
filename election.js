07630353e2b1b4dca28dd400260597f57da34e12
Your Name <arssonist@yahoo.com>
2016-09-17 17:44:02 -0400

    ajax, and loop created, but ability to insert into list does not work

--- election.js
+++ election.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
+    // $('').click(function(){
+      $.ajax({
+        url:'https://bb-election-api.herokuapp.com/',
+        method:'GET',
+        data: {},
+        dataType:'json'
+      }).done(function(responseData){
+// create candidates list
+        var candidates =
+        responseData['candidates'];
 
-  // Imagination!
+        console.log(candidates);
+// create ul, not sure if here of in HTML
+      var candidateContainer = $('<ul>');
+        // loop through candidates
+      for (var i = 0; i < candidates.length; i++){
+        // clark's code
+      $('<li>').html("Name: " + candidates[i].name + " Votes: " + candidates[i].votes).appendTo
+      }
+      // candidates.forEach(function(i){
+      //   console.log(candidates[i])
+      // // });
 
+    });
+    // });
 });
