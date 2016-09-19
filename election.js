f6ba99b5a3a0d10d5130aca7659acf44c781b332
Your Name <arssonist@yahoo.com>
2016-09-19 14:39:12 -0400

    create li and loop to fill in li. Name and votes are displaying to screen

--- election.js
+++ election.js
@@ -10,14 +10,23 @@ $(document).ready(function() {
         var candidates =
         responseData['candidates'];
 
-        console.log(candidates);
+        // console.log(candidates);
 // create ul, not sure if here of in HTML
       var candidateContainer = $('<ul>');
         // loop through candidates
       for (var i = 0; i < candidates.length; i++){
         // clark's code
-      $('<li>').html("Name: " + candidates[i].name + " Votes: " + candidates[i].votes).appendTo
+      // $('<li>').html("Name: " + candidates[i].name + " Votes: " + candidates[i].votes).appendTo
+
+  // create new li
+      var newli = $('<li>');
+  // change the parts of li to name and votes, use the i to change on each one
+      newli.text(" " + candidates[i].name + " " + candidates[i].votes);
+  // attached the new li to the entire container
+      newli.appendTo(candidateContainer)
+      // repalce old ul with new ul
       }
+      $('#ul-id').html(candidateContainer);
       // candidates.forEach(function(i){
       //   console.log(candidates[i])
       // // });
